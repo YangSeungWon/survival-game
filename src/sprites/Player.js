ff55bd620af435fc8c2565cc7254ab03d26af638
whysw <35984562+YangSeungWon@users.noreply.github.com>
2024-10-28 22:50:07 +0900

    fix player move speed

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -15,21 +15,38 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         scene.physics.add.existing(this);
         this.setCollideWorldBounds(true);
         this.setBounce(1);
+
+        this.speed = 160; // 이동 속도를 변수로 설정
     }
 
     update(cursors) {
         this.setVelocity(0);
 
+        let velocityX = 0;
+        let velocityY = 0;
+
         if (cursors.left.isDown) {
-            this.setVelocityX(-160);
+            velocityX = -1;
         } else if (cursors.right.isDown) {
-            this.setVelocityX(160);
+            velocityX = 1;
         }
 
         if (cursors.up.isDown) {
-            this.setVelocityY(-160);
+            velocityY = -1;
         } else if (cursors.down.isDown) {
-            this.setVelocityY(160);
+            velocityY = 1;
         }
+
+        // 벡터의 길이를 계산
+        const length = Math.sqrt(velocityX * velocityX + velocityY * velocityY);
+
+        // 벡터의 길이가 0이 아닐 때만 정규화
+        if (length !== 0) {
+            velocityX /= length;
+            velocityY /= length;
+        }
+
+        // 정규화된 벡터에 speed를 곱하여 속도 설정
+        this.setVelocity(velocityX * this.speed, velocityY * this.speed);
     }
 }
